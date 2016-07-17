8018b9f35a52fe0a92567c4faf9b57c6def83248
Kuznetsov Sergey <azon.sk@gmail.com>
2016-07-17 21:07:06 +0300

    basic spring project

--- src/main/java/io/khasang/qb/controller/AppController.java
+++ src/main/java/io/khasang/qb/controller/AppController.java
@@ -1,4 +1,21 @@
 package io.khasang.qb.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class AppController {
+    @RequestMapping("/")
+    public String hello(Model model) {
+        model.addAttribute("hello", "basic spring");
+        return "hello";
+    }
+
+    @RequestMapping("/hello")
+    public String next(Model model) {
+        model.addAttribute("row", "row");
+        return "next";
+    }
+
 }
---
+++ web/WEB-INF/views/hello.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>QB! Welcome page!</title>
+</head>
+<body>
+<h1>Hello!</h1>
+<p>${hello}</p>
+</body>
+</html>
