a21dd098199692f4e5b8e4af75e8fed6d093a51d
Igor Prus <amitator@gmail.com>
2016-08-05 17:53:58 +0300

    added EncryptedPasswordView

--- src/main/java/io/khasang/qb/controller/LoginController.java
+++ src/main/java/io/khasang/qb/controller/LoginController.java
@@ -1,7 +1,20 @@
 package io.khasang.qb.controller;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController {
+
+    @RequestMapping(value = {"hello/{name}"}, method = RequestMethod.GET)
+    public ModelAndView hello(@PathVariable("name") String name) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("encryptedPasswordView");
+        modelAndView.addObject("crypt", new BCryptPasswordEncoder().encode(name));
+        return modelAndView;
+    }
 }
---
+++ web/WEB-INF/views/encryptedPasswordView.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Encrypted password</title>
+</head>
+<body>
+<h1>password</h1>
+<p>${crypt}</p>
+</body>
+</html>
