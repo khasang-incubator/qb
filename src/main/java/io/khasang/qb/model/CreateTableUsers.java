756f3763c6bab0a2c105274c293898e44f02de5a
Igor Prus <prus.igor@gmail.com>
2016-08-06 16:23:22 +0300

    edited password VARCHAR(45) to VARCHAR(60) acoordint to this
     "A common mistake, the length of the “password” column (users table) is less  than 60, for example, password VARCHAR(45), and some databases will truncate the data automatically. So, you always get the warning “Encoded password does not look like BCrypt”.
    
    To solve it, make sure the length of “password” column is at least 60."

--- src/main/java/io/khasang/qb/model/CreateTableUsers.java
+++ src/main/java/io/khasang/qb/model/CreateTableUsers.java
@@ -17,7 +17,7 @@ public class CreateTableUsers {
             jdbcTemplate.execute("DROP TABLE IF EXISTS users;");
             jdbcTemplate.execute("CREATE TABLE users (\n" +
                     "    username     VARCHAR(45) NOT NULL,\n" +
-                    "    password    VARCHAR (45) NOT NULL,\n" +
+                    "    password    VARCHAR (60) NOT NULL,\n" +
                     "    enabled int NOT NULL , \n" +
                     "    PRIMARY KEY(username)\n" +
                     ");");
