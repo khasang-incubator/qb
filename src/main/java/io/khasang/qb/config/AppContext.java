ae11cda6cb90ce808c092c78d61701786c732840
sadpanda <saadpanda@users.noreply.github.com>
2016-08-11 19:00:03 +0300

    add spring security

--- src/main/java/io/khasang/qb/config/AppContext.java
+++ src/main/java/io/khasang/qb/config/AppContext.java
@@ -9,12 +9,18 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 
 @Configuration
 @PropertySource(value = {"classpath:util.properties"})
+@PropertySource(value = {"classpath:auth.properties"})
 public class AppContext {
     @Autowired
     Environment environment;
+
+    @Autowired
+    HibernateConfig hibernateConfig;
 
     @Bean
     public Message message() {
@@ -26,6 +32,15 @@ public class AppContext {
         JdbcTemplate jdbcTemplate = new JdbcTemplate();
         jdbcTemplate.setDataSource(dataSource());
         return jdbcTemplate;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        JdbcDaoImpl jdbcImpl = new JdbcDaoImpl();
+        jdbcImpl.setDataSource(hibernateConfig.dataSource());
+        jdbcImpl.setUsersByUsernameQuery(environment.getRequiredProperty("usersByQuery"));
+        jdbcImpl.setAuthoritiesByUsernameQuery(environment.getRequiredProperty("rolesByQuery"));
+        return jdbcImpl;
     }
 
     @Bean
