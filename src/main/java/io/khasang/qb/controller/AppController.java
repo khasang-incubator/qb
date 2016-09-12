414b6ec1397ee81e0f610f1e66b29aae9f89c877
browseup <an@browseup.ru>
2016-09-12 21:00:36 +0300

    Начальный проект MVC

--- src/main/java/io/khasang/qb/controller/AppController.java
+++ src/main/java/io/khasang/qb/controller/AppController.java
@@ -1,4 +1,15 @@
 package io.khasang.qb.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class AppController {
+
+    @RequestMapping("/")
+    public String hello(Model model) {
+        return "hello";
+    }
+
 }
---
+++ web/WEB-INF/views/hello.jsp
@@ -0,0 +1,11 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<html>
+  <head>
+    <title>HELLO</title>
+  </head>
+  <body>
+  О ЧУДО !!!
+  </body>
+</html>
