a224c97645da3b51ff9e89d10802bdde3195f8d9
anshafs <anhafs@gmail.com>
2016-09-12 11:08:52 -0700

    создание таблиц БД

---
+++ src/main/recources/tables.sql
@@ -0,0 +1,23 @@
+﻿drop table if exists users;
+create table users (
+id serial primary key,
+login varchar(50),
+password varchar(50),
+role_id int);
+
+drop table if exists roles;
+create table roles (
+id serial primary key,
+name varchar(50));
+
+drop table if exists questions;
+create table questions (
+id serial primary key,
+question text);
+
+drop table if exists answers;
+create table answers (
+id serial primary key,
+question_id int,
+answer text,
+is_correct int);
