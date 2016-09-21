e0920577481983e10576b7a2d180240c6b7cfee0
anshafs <anhafs@gmail.com>
2016-09-21 00:54:18 -0700

    id создаются в базе

--- src/main/java/io/khasang/qb/entity/Answer.java
+++ src/main/java/io/khasang/qb/entity/Answer.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 public class Answer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column
